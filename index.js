eee667237e3d986ff66a80199aed95308e599d03
SANAL C T <137063573+SANAL-C-T@users.noreply.github.com>
2024-08-10 20:28:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var b1=document.getElementById("bt1");
 var b2=document.getElementById("bt2");
 
 
-b1.addEventListener("click",changebg1);
+b1.addEventListener("mouseenter",changebg1);
 b2.addEventListener("mouseenter",changebg2);
-b1.addEventListener("mouseleave",previousbg1);
-b2.addEventListener("mouseleave",previousbg2);
+b1.addEventListener("mouseout",previousbg1);
+b2.addEventListener("mouseout",previousbg2);
 
 function changebg1(){
-    document.getElementById("bt1").style.backgroundColor="#000";
+    b1.style.backgroundColor="#000";
 
 }
 
 function changebg2(){
-    b2.style.backgroundColor = "rgba(249, 245, 245, 0.976)";
+    b2.style.backgroundColor = "#000";
 
 }
 
@@ -25,6 +25,8 @@ function previousbg2(){
     b1.style.backgroundColor="rgba(255, 255, 255, 0.158) ";
     
 }
+
+
 
 
 let open=false;
@@ -91,6 +93,3 @@ function loadCompayMenu(){
         loadCompany=false;
     }
 }
-
-
-
